270d379583a73aa8e36ce5649bd7ddb037880181
Süleyman Begic <begic@software.tirol>
2023-11-24 15:34:20 +0100

    install ef

---
+++ TicTacToe.Data/BloggingContextFactory.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+
+namespace TicTacToe.Data;
+
+public class BloggingContextFactory : IDesignTimeDbContextFactory<DataBaseContext>
+{
+    public DataBaseContext CreateDbContext(string[] args)
+    {
+        var optionsBuilder = new DbContextOptionsBuilder<DataBaseContext>();
+        optionsBuilder.UseSqlite(@"DataSource=MyDataBase.db;");
+
+        return new DataBaseContext(optionsBuilder.Options);
+    }
+}
---
+++ TicTacToe.Data/DataBaseContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace TicTacToe.Data;
+
+public class DataBaseContext : DbContext
+{
+    
+    
+    
+    public DataBaseContext(DbContextOptions<DataBaseContext> options) : base(options)
+    {
+        
+    }
+    
+    protected override void OnConfiguring(DbContextOptionsBuilder options)
+    {
+        options.UseSqlite(@"DataSource=MyDataBase.db;");
+    }
+}
