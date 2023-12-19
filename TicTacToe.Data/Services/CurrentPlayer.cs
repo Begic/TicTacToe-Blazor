ac62ad7cc4f01b6aca5ab49d00be34163966ab3f
Süleyman Begic <begic@software.tirol>
2023-12-19 15:06:24 +0100

    sync

---
+++ TicTacToe.Data/Services/CurrentPlayer.cs
@@ -0,0 +1,11 @@
+﻿namespace TicTacToe.Data.Services;
+
+public class CurrentPlayer
+{
+    public bool PlayVsHuman { get; set; }
+
+    public string NamePlayerOne { get; set; }
+    public string NamePlayerTwo { get; set; }
+    public string ColorPlayerOne { get; set; }
+    public string ColorPlayerTwo { get; set; }
+}
