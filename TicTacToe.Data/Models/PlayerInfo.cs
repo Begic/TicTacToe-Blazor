377bf8b237f7eebd7959aebd3dabab719cc2f6b3
Süleyman Begic <begic@software.tirol>
2024-01-09 14:26:30 +0100

    playercolor done

--- TicTacToe.Data/Models/PlayerInfo.cs
+++ TicTacToe.Data/Models/PlayerInfo.cs
@@ -4,8 +4,8 @@ public class PlayerInfo
 {
     public string NamePlayerOne { get; set; }
     public string NamePlayerTwo { get; set; }
-    public string? ColorPlayerOne { get; set; } = "#000000";
-    public string? ColorPlayerTwo { get; set; } = "#000000";
+    public string? ColorPlayerOne { get; set; } = "#ff0000";
+    public string? ColorPlayerTwo { get; set; } = "#ff0000";
     
     public bool PlayVsHuman { get; set; }
 }
--- TicTacToe.Data/Services/CurrentPlayer.cs
+++ TicTacToe.Data/Services/CurrentPlayer.cs
@@ -6,6 +6,6 @@ public class CurrentPlayer
 
     public string NamePlayerOne { get; set; }
     public string NamePlayerTwo { get; set; }
-    public string? ColorPlayerOne { get; set; } 
-    public string? ColorPlayerTwo { get; set; } 
+    public string? ColorPlayerOne { get; set; } = "#ff0000";
+    public string? ColorPlayerTwo { get; set; } = "#ff0000";
 }
