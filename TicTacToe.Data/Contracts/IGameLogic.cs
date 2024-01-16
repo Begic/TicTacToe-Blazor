56ac30486cdce9ece5af136131c36b2afb6f6a0b
Süleyman Begic <begic@software.tirol>
2024-01-16 20:59:58 +0100

    KI / ML.NET implementieren

--- TicTacToe.Data/Contracts/IGameLogic.cs
+++ TicTacToe.Data/Contracts/IGameLogic.cs
@@ -5,4 +5,5 @@ namespace TicTacToe.Data.Contracts;
 public interface IGameLogic
 {
     Player CheckWinner(List<int> pointsPlayerO, List<int> pointsPlayerX);
+    int GetNextMove(List<int> playerPointsX, List<int> playerPointsO);
 }
--- TicTacToe.Data/Services/GameLogic.cs
+++ TicTacToe.Data/Services/GameLogic.cs
@@ -8,17 +8,53 @@ namespace TicTacToe.Data.Services
     {
         public Player CheckWinner(List<int> pointsPlayerO, List<int> pointsPlayerX)
         {
-            if (WinningCombination.Combination.Any(z => z.All(pointsPlayerX.Contains) && z.Count == pointsPlayerX.Count))
+            if (WinningCombination.Combination.Any(z =>
+                    z.All(pointsPlayerX.Contains) && z.Count == pointsPlayerX.Count))
             {
                 return Player.X;
             }
 
-            if (WinningCombination.Combination.Any(z => z.All(pointsPlayerO.Contains) && z.Count == pointsPlayerO.Count))
+            if (WinningCombination.Combination.Any(z =>
+                    z.All(pointsPlayerO.Contains) && z.Count == pointsPlayerO.Count))
             {
                 return Player.O;
             }
 
             return Player.Default;
+        }
+        
+        public int GetNextMove(List<int> playerPointsX, List<int> playerPointsO)
+        {
+            var blockingMove = WinningCombination.Combination
+                .Where(c => c.Count(p => playerPointsX.Contains(p)) == 2)
+                .SelectMany(c => c)
+                .FirstOrDefault(p => !playerPointsX.Contains(p) && !playerPointsO.Contains(p));
+
+            if (blockingMove != 0)
+            {
+                return blockingMove;
+            }
+
+            var availableCombinationsForO = WinningCombination.Combination
+                .Where(c => !c.Any(p => playerPointsX.Contains(p)))
+                .ToList();
+
+            var bestCombinationForO = availableCombinationsForO
+                .OrderByDescending(c => c.Count(p => playerPointsO.Contains(p)))
+                .FirstOrDefault();
+
+            if (bestCombinationForO != null)
+            {
+                var moveForO = bestCombinationForO
+                    .Except(playerPointsO)
+                    .FirstOrDefault();
+
+                if (moveForO != 0)
+                {
+                    return moveForO;
+                }
+            }
+            return -1;
         }
     }
 }
